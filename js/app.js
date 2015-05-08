e65a2247360a3a28a909f25b36b712a18ab6cbf4
alwesam <alwesam@gmail.com>
2015-05-08 00:58:29 -0700

    major refractoring done, main functionalities are completed

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@
 var map;
 
 var initializeMap = function () {
+    var canvas = document.getElementById('map-canvas');
     var mapOptions = {
     	//vancouver coordinates
        	center: { lat: 49.27, lng: -123.07},
         zoom: 15
     };
-    map = new google.maps.Map(document.getElementById('map-canvas'),
-            			mapOptions);
+    map = new google.maps.Map(canvas, mapOptions);
 };
 
 /** 
@@ -23,123 +23,78 @@ var initializeMap = function () {
 var listOfPlaces = [
     {
         name: "St. Augustine's Brewery",
-        address: "2360 Commercial Dr, Vancouver, BC",        
-        description: 'fantastic',
-        visible: false,
-        zoom: false,
+        address: "2360 Commercial Dr, Vancouver, BC",
         keywords: ["Beer","Bar","Pub"]
     },
     {
         name: "The Strom Crow Tavern",
-        address: "1305 Commercial Dr, Vancouver, BC",        
-        description: 'fantastic',
-        visible: false,
-        zoom: false,
+        address: "1305 Commercial Dr, Vancouver, BC",
         keywords: ["Beer","Bar","Pub"]
     },
     {
         name: "The Charlatan",
-        address: "1447 Commercial Dr, Vancouver, BC",        
-        description: 'fantastic',
-        visible: false,
-        zoom: false,
+        address: "1447 Commercial Dr, Vancouver, BC",
         keywords: ["Beer","Bar","Pub"]
     },
     {
         name: "Havana",
         address: "1212 Commercial Dr, Vancouver, BC",        
         description: 'fantastic',
-        visible: false,
-        zoom: false,
         keywords: ["Food"]
     },
     {
         name: "Turk's Coffee Bar",
-        address: "1276 Commercial Dr, Vancouver, BC",        
-        description: 'fantastic',
-        visible: false,
-        zoom: false,
+        address: "1276 Commercial Dr, Vancouver, BC",
         keywords: ["Coffee"]
     }
 ];
  
 
-ko.bindingHandlers.mapper = {
-    /* element: DOM element involved in the binding
-     * valueAccessor: A JavaScript function that you can call to get the 
-     * current model property that is involved in this binding
-     * allBindings: JavaScript object that you can use to access all the model values bound to the DOM element
-     */
-    init: function (element, valueAccessor, allBindingsAccessor, viewModel, BindingContext) {
-        // This will be called when the binding is first applied to an element
-        // Set up any initial state, event handlers, etc. here
-       
-        var putMarker = function (place) { 
+/* references current marker */
+var marker = function (data) {    
+
+    self = this;
+    self.name = ko.observable(data.name);
+    self.address = ko.observable(data.address);
+
+    var titleString = String(data.name);    
+
+    var putMarker = function (place) { 
 
             var marker = new google.maps.Marker({
-                    map: allBindingsAccessor().mapper,
+                    map: map,
                     position: place.geometry.location,
-                    title: allBindingsAccessor().name()
+                    title: titleString
             });
-
-            viewModel.mapMarker = marker;
-
-            //set it to false initially, don't show marker
-            marker.setVisible(allBindingsAccessor().visible());
 
             var infoWindow = new google.maps.InfoWindow({
-              content: allBindingsAccessor().name()
+              content: titleString
             });
 
-            viewModel.infoMarker = infoWindow;    
-
             google.maps.event.addListener(marker,'click',function() {          
-                infoWindow.open(map,marker);
+                infoWindow.open(map, marker);
             }); 
 
-            //zoomlogic TODO
-            if(allBindingsAccessor().zoom()) {
-                map.setZoom(18);
-                map.setCenter(viewModel.mapMarker.getPosition());
-                map.panTo(viewModel.mapMarker.getPosition());                
-                viewModel.infoMarker.open(map,viewModel.mapMarker);
-            }
+            markers.push(marker);
+            infoMarkers.push(infoWindow);
+    };
 
-            //markers.push(marker);
-        };
+    var service = new google.maps.places.PlacesService(map);        
+    //the search request object
+    var request = {query: self.address()};
 
-        var service = new google.maps.places.PlacesService(allBindingsAccessor().mapper);        
-      // the search request object
-        var request = {query: allBindingsAccessor().address()};
+    service.textSearch(request, function (results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            putMarker(results[0]);
+        }
+    }); 
 
-        service.textSearch(request, function (results, status) {
-            if (status == google.maps.places.PlacesServiceStatus.OK) {
-                putMarker(results[0]);
-            }
-        }); 
-
-    },
-
-    update: function (element, valueAccessor, allBindingsAccessor, viewModel, BindingContext) {
-        // This will be called once when the binding is first applied to an element, and again whenever any observables/computeds that are accessed change/Update the DOM element based values here.
-        //clickable     
-      
-    }
 };
 
 //var markers = ko.observableArray([]);
-
-/* references current marker */
-var marker = function (data) {    
-    self = this;
-    this.name = ko.observable(data.name);
-    this.address = ko.observable(data.address);
-    this.isVisible = ko.observable(data.visible);
-    this.inZoom = ko.observable(data.zoom);
-};
-
-//not good for now
-var count = 0;
+//TODO review the following!!!!
+var markers = [];
+var infoMarkers = [];
 
 var viewModel = function () {
     //this here refers to ViewModel scope
@@ -150,7 +105,6 @@ var viewModel = function () {
     //create a list of markers based on list
     listOfPlaces.forEach(function (loc) {
         self.markerList.push( new marker(loc) );
-        //self.oneMarker( new marker(loc) );
     });
 
     self.inputAddress = ko.observable("");
@@ -158,48 +112,53 @@ var viewModel = function () {
     this.searchAddress = function () {
         //check first if searchAddress is not null
         if(!self.inputAddress() || self.inputAddress().length === 0){
-            alert('enter somethign');
+            alert('Invalid Entry');
         } else {
                
-            count = 0; 
-
             //reset
-            self.markerList.splice(0); 
+            self.markerList.splice(0);
+
+            //remove markers
+            for (var i = 0; i < markers.length; i++) {
+                markers[i].setMap(null);
+            }
+            //then truncate the array
+            markers.splice(0);
 
             listOfPlaces.forEach(function (loc) {
                 //display marker
                 //search based on address and name
                 var s = (loc.name+loc.address).toLowerCase();
                 var d = self.inputAddress().toLowerCase();
-                //console.log(s);
-                //console.log(self.inputAddress());
                 if(s.indexOf(d) > -1) {
                     self.markerList.push(new marker(loc) );                    
                 }
             });       
 
-            console.log(self.markerList().length)     
         }        
         
     };
 
-    setInterval( function () {
-
-            if(count < self.markerList().length) {
-
-                var address = self.markerList()[count];
-                address.isVisible(true);
-                self.oneMarker(address); 
-                count++;
-
-            } 
-        }, 
-        0);
-
     this.zoom = function (item) {
-        
-        this.inZoom(true);
-        self.oneMarker(item);
+
+        var searchIndex = function () {
+            for (var i = 0; i < markers.length; i++) {
+                if(markers[i].title === item.name())
+                    return i;
+            }
+            return -1;
+        };
+
+        var id = searchIndex();        
+
+        if(id > -1) {
+            map.setZoom(18);
+            map.panTo(markers[id].getPosition());                
+            infoMarkers[id].open(map, markers[id]);
+            //self.oneMarker(item);
+        } else {
+            alert('marker not found');
+        }                
 
     };
 
@@ -207,11 +166,9 @@ var viewModel = function () {
 
 //wait till page is loaded
 document.addEventListener('DOMContentLoaded', function() {
-
     //start
     initializeMap();
     //the bindings connect the view and the model
     ko.applyBindings(new viewModel());
-
 });
 
