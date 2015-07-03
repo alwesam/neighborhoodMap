143b9d5af5c0bb50f77ad0173e76a339d31d9f15
alwesam <alwesam@gmail.com>
2015-07-03 00:22:02 -0700

    add README file, fixed map resizing function, and cleaned up html and css files

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# neighborhoodMap
+# Vancouver Landmarks
+In this simple navigable web app, I listed few (of many) favorite spots in my
+hometown of Vancouver, BC.  The app mainly consists of an embeded google map
+along with a clickable list view and on the map.
+
+## Requirements
+
+- A web broswer with Javascript enabled
+	- This application was tested on the following web browsers:
+		- Chrome version 41
+		- Firefox version version 37
+
+## Running the application
+
+1. Download the [project files](http://github.com/alwesam/neighborhoodMap).
+2. The directory structure should be as follows:
+	* css/
+		- style.css
+	* js
+		- app.js 
+	* index.html
+	* README.md
+3. Launch *index.html* in a web browser of your choice (please check above requirements).
+4. The app page consists of two main columns.  The left columns consists a list
+   view of clickable items of places.  On the top left, you can find a search
+   form to filter through the list.  The right side include a google map with
+   the markers listed on the list view.
+5. When either the marker or a list view item is being clicked, an Ajax request
+   is made to Wikipedia and displays content related to the clicked item on the
+   bottom left.
+6. The search form can be used to filter through the list item.  The number of
+   filtered items will be reflected on the markers shown on the map.
+7. The *show all* button can be clicked to display all of the markers and items. 
--- js/app.js
+++ js/app.js
@@ -1,25 +1,30 @@
+/**
+ * Authored by Wesam Al-Haddad
+ * June 2015
+ * This code was linted using JSHint
+ * /
+ 
 /*constants*/
 var _ZOOM_CLOSE   =  17;
-var _CITY = "Vancouver";
-var _PROV = "BC";
 var _GREEN = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
 var _RED = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
- /*global variable*/
+/*map is a global variable*/
 var map;
 
 /** 
- * Will contain all markers of interest
+ * listOfPlaces is an array of Objects
  * Each array element (obejct) will contain the following
- * Coordinates: longitude and latitude (remove)
- * Address: use google search
- * Category: [restaurant, supermarket, library, bar, hotspot, ]
- * Description: custom description of place
- * TODO: include in a separate JSON file
+ * name: Name of landmark 
+ * Address: Address (street address or postal code) 
  */
 var listOfPlaces = [
     {
-        name: "BC Place Stadium",
-        address: "777 Pacific Boulevard"
+        name: "Capilano Suspension Bridge",
+        address: "3735 Capilano Rd, North Vancouver"
+    },
+    {
+        name: "Pacific Spirit Regional Park",
+        address: "4915 W. 16th Ave"
     },
     {
         name: "Granville Island",
@@ -47,62 +52,65 @@ var listOfPlaces = [
     }
 ];
 
-/* references current marker */
+/* This function will be
+ * run only once  */
 var markerObj = function (data, scope) {    
 
+    /*assigning observables*/
     var self = this;
     self.name = ko.observable(data.name);
     self.address = ko.observable(data.address);
+    /* listview item is visible by default */
     self.visible = ko.observable(true);
     
-   // self.visible.subscribe(function (visible) {
-   //   self.markerObj.setVisible(visible);
-   // });
-
     var titleString   = String(self.name());    
     var addressString = String(self.address());    
 
     var placeMarker = function (place) {
-
-            var lat = place.geometry.location.lat();  // latitude from the place service
-            var lon = place.geometry.location.lng();  // longitude from the place service
-            //var name = placeData.formatted_address;   // name of the place from the place service
+            
+            /*coordiantes from place service*/
+            var lat = place.geometry.location.lat();  
+            var lon = place.geometry.location.lng(); 
 
             var marker = new google.maps.Marker({
                     map: map,
                     position: place.geometry.location,
-                    title: titleString
+                    title: titleString,
+                    icon: _RED
             });
-
-            var infoWindow = new google.maps.InfoWindow({
-              content: titleString//+', '+addressString
+            
+            /* set marker infobox as property of marker*/
+            marker.infoBox = new google.maps.InfoWindow({
+              content: titleString +', '+ addressString
             });
-
-            marker.infoBox = infoWindow;
+            
+            /* set name and address as properties of marker.  These properties
+             * in addition to content will be tied to the oneMarker observable */
             marker.name = titleString;
             marker.address = addressString;
-            //add other paramters
+
             /*Set content to title and address for now*/
             marker.content = marker.infoBox.content;
-            /*Add event clicklistener*/
+
+            /*Add event clicklistener for clicking marker*/
             google.maps.event.addListener(marker,'click',function() {          
                 openMarker(marker, scope);
             }); 
-
-            resizeMap(lat,lon);
+            
+            /*resize and center map as markers are added*/
+            resizeMap(window.mapBounds, lat,lon);
            
            /*return marker to be added as a property of the markerList object*/
            return marker;
     };
 
+    /* use Google Place Services to find coordinates from address input*/
     var service = new google.maps.places.PlacesService(map);        
-    //the search request object
     var request = {query: self.address()};
-
     service.textSearch(request, function (results, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
             /*google marker object will be a property of the knockout marker
-             * object.  This way, the listview items and the corrsponding
+             * object.  This way, the listview items and the corresponding
              * markers on the map will be tied to one object*/
             self.markerObj = placeMarker(results[0]);
         }
@@ -110,42 +118,58 @@ var markerObj = function (data, scope) {
 
 };
 
+/* *
+ * This function is called when a listview item or a marker is clicked
+ * The input parameters are the corresponding marker and the viewModel
+ * reference (scope)
+ */
 var openMarker = function (marker, scope) {
 
+    /*The marker title or the landmark name will be passed as the search query
+     * on Wikipedi's API link*/
     var searchItem = marker.title;
     var wikiURL = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' 
                   + searchItem 
                   + '&format=json&callback=wikiCallback';
 
     /*set a timeout of 8 seconds for the AJAX request to complete
-     * otherwise
+     * otherwise, set marker.title to fail
      */
     var requestTimeout = setTimeout(function () {
        marker.title = 'Failed to fetch Wikipedia link';
     }, 8000);
 
+    /* Send an AJAX request to Wikipedia*/
     $.ajax({
        url: wikiURL, 
+       /*jsonp datatype is specified to be able to pass functions in JSON
+        * request*/
        dataType: "jsonp",
        success: function(data) {
+             /*Fetch list of articles*/
              var articles = data[1];            
+             /*Fetch first (likely) article*/
              var article = articles[0];
+             /*Fetch snippet of wikipedia article */
              var snippet = data[2][0];
+             /*build a url of the wikipedia article of the landmark*/
              var url = 'http://en.wikipedia.org/wiki/' + article;
-             marker.content = '<strong>'+article+'</strong><br>'+
-                              '<p>'+snippet+'</p>'+                                   
+             /*assign content with snippet*/
+             marker.content = '<p>'+snippet+'</p>'+                                   
                               '<a target="_blank" href="'+url+'">Wikipedia article</a>';                                   
     
-             //open marker info box
+             /*open marker info box*/
              marker.infoBox.open(map, marker);    
-             //display content on sidebar
+             /*display content on sidebar*/
              scope.oneMarker(marker);
-
+             /*clear requestTimeout*/
              clearTimeout(requestTimeout); 
         }        
     });
-
+  
+    /*set selected landmark's marker to green*/
     marker.setIcon(_GREEN);
+    /*zoom closer the selected landmark*/
     map.setZoom(_ZOOM_CLOSE);
     map.panTo(marker.getPosition());
 
@@ -153,31 +177,49 @@ var openMarker = function (marker, scope) {
 
 var viewModel = function () {
 
-    //this here refers to ViewModel scope
+    /*this here refers to ViewModel scope*/
     var self = this;
-    
+   
+    /* oneMarker observable will track detailed information of a selected
+     * marker/listview item*/ 
     self.oneMarker = ko.observable();
 
-    //create a list of markers based on list
+    /*create an observable array of markers based off list*/
     self.markerList = ko.observableArray(
         listOfPlaces.map(function (loc) {
           return new markerObj(loc, self);
         })
-      );
+     );
 
+    /*search text field is set to empty string*/ 
     self.inputAddress = ko.observable("");
     
+    /*searchAddress function observable is called when selecting search button
+     * or hitting enter from the form text field*/
     this.searchAddress = function () {
+        /*check validity of input search query*/
         if(!self.inputAddress() || self.inputAddress().length === 0) {
             alert('Invalid Entry');
         } 
         else {
             self.resetMap();
             var d = self.inputAddress().toLowerCase();
+            /*instantiate a new LatLng bounds object in order to resize maps
+             * based on filtered available markers*/
+            var bounds = new google.maps.LatLngBounds();
             self.markerList().forEach(function (m) {
+                /* search either name or address of location*/
                 var s = (m.name()+' '+m.address()).toLowerCase();
+                /*if a search query matches a pattern in either the location's
+                 * name or address, return true and set listview item and marker
+                 * visible; if false set listview item and marker invisible */
                 m.visible((s.indexOf(d) > -1));
                 m.markerObj.setVisible(s.indexOf(d) > -1);
+                /*resize map to zoom to selected items*/
+                if (s.indexOf(d) > -1)
+                  resizeMap(bounds, 
+                      m.markerObj.getPosition().lat(), 
+                      m.markerObj.getPosition().lng());
             });
         }        
     };
@@ -190,8 +232,11 @@ var viewModel = function () {
       }
     });
 
+    /*another jquery snippet to change cursor to pointer when hovering over
+     * listview*/
     $('.list-view').css('cursor', 'pointer');
 
+    /*The selectMarker function observable is called when a listview item is clicked*/
     this.selectMarker = function (item) {
 
         self.resetMap();
@@ -215,8 +260,12 @@ var viewModel = function () {
 
     };
 
+    /*This function resets the index page and returns it to its initial state*/
     this.resetMap = function () {
         var marker = self.markerList();
+
+        resizeMap(window.mapBounds);
+
         marker.forEach(function(m){
           //close all infoboxes if they're still open
           m.markerObj.infoBox.close();
@@ -224,43 +273,51 @@ var viewModel = function () {
           m.visible(true);
           //set marker visible to true
           m.markerObj.setVisible(true);
+          //set marker icon to red
           m.markerObj.setIcon(_RED);
-          resizeMap(m.markerObj.getPosition().lat(), 
-                    m.markerObj.getPosition().lng());
         });
 
+        //emtpy content
         self.oneMarker(null);
 
     };
 
 };
 
-var resizeMap = function (lat, lon) {
-
-	var bounds = window.mapBounds; 
-	bounds.extend(new google.maps.LatLng(lat, lon));
+/**This function resizes and centers the map based on incoming location
+ * coordinates */
+var resizeMap = function (bounds, lat, lon) {
+  
+  if(lat !== undefined && lon !== undefined)
+	  bounds.extend(new google.maps.LatLng(lat, lon));
 	// fit the map to the new marker
 	map.fitBounds(bounds);
 	// center the map
 	map.setCenter(bounds.getCenter());
 
+  /*Enforce max zoom level */
+  if (map.getZoom() > _ZOOM_CLOSE)
+    map.setZoom(_ZOOM_CLOSE);
+
 };
 
+/** This call-back function is executed after loading the google map on the
+ * app's index page  */
 var initializeMap = function () {
-
+    /*grab the element where the map canvas will be placed*/
     var canvas = $('#map-canvas');
 
     /*grab the JS vanilla portion of the jquery wrap set*/
     map = new google.maps.Map(canvas[0]);
 
-    /* Sets the boundaries of the map based on pin locations*/
+    /* Sets the boundaries of the map based on marker locations*/
     window.mapBounds = new google.maps.LatLngBounds();
 
-    /*apply ko bindings*/
+    /*apply ko bindings and instantiates a new viewModel*/
     ko.applyBindings(new viewModel());
     
     /**
-     * The following code to keep map centered when reizing window was take from
+     * The following code to keep map centered when reizing window was taken from
      * the following stackoverflow link:
      * http://stackoverflow.com/questions/23947904/keep-google-maps-centered-when-window-resize
      */
@@ -282,9 +339,8 @@ var initializeMap = function () {
 
 };
 
-//wait till page is loaded
+/*wait till DOM of index html page is loaded before initializing map*/
 $(document).ready(function() {
   var link = 'http://maps.googleapis.com/maps/api/js?libraries=places&callback=initializeMap';
   $('body').append("<script src="+link+"></script>");
 }); 
-
